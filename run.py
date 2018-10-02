63909a5da3b8595d44a7e91e3916e74ac3cbfce8
metheoryt <metheoryt@gmail.com>
2018-10-02 11:07:00 +0600

    again fix

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 from app import updater
 import logging
 import cfg
@@ -13,6 +15,16 @@ log = logging.getLogger(__name__)
 
 
 if __name__ == '__main__':
+
+    for i in range(5):
+        try:
+            engine.connect()
+        except Exception as e:
+            if i == 4:
+                raise
+            log.warning(f'cannot connect to db, will try {4-i} more times ({e!r})')
+            sleep(3)
+
     Base.metadata.create_all(engine)
     if not Exchange.q.count():
         log.info('fresh install detected, filling DB with static data')
